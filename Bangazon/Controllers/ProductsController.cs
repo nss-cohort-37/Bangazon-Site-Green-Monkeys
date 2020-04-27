2dc2d5fafc43818ce745d5b9af8d60275776f1a6
Case Scally <caseScally@gmail.com>
2020-04-27 15:43:00 -0700

    create product view'

---
+++ Bangazon/Controllers/ProductsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Bangazon.Data;
+using Bangazon.Models;
+
+namespace Bangazon.Controllers
+{
+    [Authorize]
+    public class ShoppingItemsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public ShoppingItemsController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        // GET: Products
+        public async Task<ActionResult> Index()
+        {
+            var user = await GetCurrentUserAsync();
+            var products = await _context.Product
+                .Where(p => p.UserId == user.Id)
+                .Include(pt => pt.ProductType)
+                .ToListAsync();
+
+            return View(products);
+        }
+
+        // GET: ShoppingItems/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ShoppingItems/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ShoppingItems/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(Product product)
+        {
+            try
+            {
+                var user = await GetCurrentUserAsync();
+                product.UserId = user.Id;
+
+                _context.Product.Add(product);
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ShoppingItems/Edit/5
+        public async Task<ActionResult> Edit(int id)
+        {
+            var item = await _context.Product.FirstOrDefaultAsync(p => p.ProductId == id);
+            var loggedInUser = await GetCurrentUserAsync();
+
+            if (item.UserId != loggedInUser.Id)
+            {
+                return NotFound();
+            }
+
+            return View(item);
+        }
+
+        // POST: ShoppingItems/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(int id, Product product)
+        {
+            try
+            {
+                var user = await GetCurrentUserAsync();
+                product.UserId = user.Id;
+
+                _context.Product.Update(product);
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ShoppingItems/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ShoppingItems/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        private Task<ApplicationUser> GetCurrentUserAsync() => _userManager.GetUserAsync(HttpContext.User);
+    }
+}
---
+++ Bangazon/Models/ProductViewModels/ProductCreateViewModel.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bangazon.Models.ProductViewModels
+{
+    public class ProductCreateViewModel
+    {
+        [Key]
+        public int ProductId { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime DateCreated { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Description { get; set; }
+
+        [Required]
+        [StringLength(55, ErrorMessage = "Title must be 55 characters")]
+        public string Title { get; set; }
+
+        [Required]
+        public double Price { get; set; }
+
+        [Required]
+        public int Quantity { get; set; }
+
+        [Required]
+        public string UserId { get; set; }
+
+        public string City { get; set; }
+
+        public bool Active { get; set; }
+        
+        [Required]
+        public string ApplicationUserId { get; set; }
+
+        public ApplicationUser User { get; set; }
+
+        [Required]
+        [Display(Name = "Product Type")]
+        public int ProductTypeId { get; set; }
+
+        public List<SelectListItem> ProductTypeOptions { get; set; }
+
+        public string ImagePath { get; set; }
+    }
+}
