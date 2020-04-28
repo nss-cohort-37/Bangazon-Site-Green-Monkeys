278d6735b6f16307b9801b74526e37e22584648b
Mac Gibbons <57641175+macgibbons@users.noreply.github.com>
2020-04-28 13:11:42 -0500

    changed order of sql file

--- Bangazon/SQL Queries/AddItemToCart.sql
+++ Bangazon/SQL Queries/AddItemToCart.sql
@@ -1,7 +1,9 @@
-﻿INSERT INTO OrderProduct (OrderId, ProductId)
+﻿INSERT INTO [Order] (DateCreated, userId, PaymentTypeId)
+VALUES (GETDATE(), '00000000-ffff-ffff-ffff-ffffffffffff', Null)
+
+INSERT INTO OrderProduct (OrderId, ProductId)
 VALUES (8, 2)
+
 INSERT INTO OrderProduct (OrderId, ProductId)
 VALUES (8, 1)
 
-INSERT INTO [Order] (DateCreated, userId, PaymentTypeId)
-VALUES (GETDATE(), '00000000-ffff-ffff-ffff-ffffffffffff', Null)
