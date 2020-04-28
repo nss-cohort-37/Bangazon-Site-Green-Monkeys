cbfb51675619925684fa1a4022a5661c542a8331
lcluischavez <57668579+lcluischavez@users.noreply.github.com>
2020-04-28 15:00:45 -0500

    Detail1

--- Bangazon/SQL Queries/AddItemToCart.sql
+++ Bangazon/SQL Queries/AddItemToCart.sql
@@ -1,7 +1,7 @@
 ﻿INSERT INTO OrderProduct (OrderId, ProductId)
-VALUES (8, 2)
+VALUES (11, 2)
 INSERT INTO OrderProduct (OrderId, ProductId)
-VALUES (8, 1)
+VALUES (11, 1)
 
 INSERT INTO [Order] (DateCreated, userId, PaymentTypeId)
-VALUES (GETDATE(), '00000000-ffff-ffff-ffff-ffffffffffff', Null)
+VALUES (GETDATE(), 'c8feab47-b9cc-4b80-b56b-f00c4431e8fe', Null)
